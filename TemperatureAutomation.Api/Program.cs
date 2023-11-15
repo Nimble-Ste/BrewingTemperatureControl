e48a07351e17d617bb692618b1399260c1da115f
Nimble-Ste <ste@nimbleapproach.com>
2023-11-15 15:13:43 +0000

    added swagger

--- TemperatureAutomation.Api/Program.cs
+++ TemperatureAutomation.Api/Program.cs
@@ -33,11 +33,11 @@ namespace TemperatureAutomation.Api
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+          //  }
 
           //  app.UseHttpsRedirection();
 
